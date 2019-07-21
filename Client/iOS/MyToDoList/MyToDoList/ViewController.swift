2dd2440e644a916c7d4132969c988a258209f9e7
Szymon Smykała <ctk7000@gmail.com>
2019-07-21 21:10:59 +0200

    Added iOS project with pods

---
+++ Client/iOS/MyToDoList/MyToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyToDoList
+//
+//  Created by Szymon Smykała on 17/07/2019.
+//  Copyright © 2019 Szymon Smykała. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
