200f2acb0c355ffbff9c6c1871398ead83489650
Szymon Smykała <ctk7000@gmail.com>
2019-07-21 21:09:42 +0200

    Update azure-pipelines.yml for Azure Pipelines (#4)
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Added Sample Test

---
+++ Server/MyToDoList/MyToDoListCoreUnitTests/SampleTest.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace MyToDoListCoreUnitTests
+{
+    [TestFixture]
+    public class SampleTest
+    {
+
+        [Test]
+        public void ExampleTest()
+        {
+            // Act
+            Assert.True(true);
+        }
+        
+    }
+}
